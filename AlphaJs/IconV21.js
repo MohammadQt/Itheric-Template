11519ec919150b58ccb2a2e7e7e543ce7895079c
MohammadQt <mohammadalhor1994@gmail.com>
2022-01-24 17:12:58 +0200

    Update and rename IconV2.js to IconV21.js

---
+++ AlphaJs/IconV21.js
@@ -0,0 +1,34 @@
+! function(icons) {
+    function iconEx() {
+        var iconEx = '';
+        icons.querySelector("body").insertAdjacentHTML("afterbegin", iconEx)
+    }
+
+    function t(icons, iconEx) {
+        var iconEx = iconEx || {},
+            t = iconEx.size ? "icon--" + iconEx.size : "",
+            iconClass = "alpha-icon" + t + "" + (iconEx["class"] || ""),
+            iconSvg = "<svg><use xlink:href='#" + icons + "' /></svg>",
+            iconSpan = "<span class='" + iconClass + "'>" + a(iconSvg, iconClass) + "</span>";
+        return iconSpan
+    }
+
+    function a(icons, iconEx) {
+        return iconEx.indexOf("spinner") > -1 ? "<span class='icon__spinner'>" + icons + "</span>" : icons
+    }
+
+    function iconClass() {
+        for (var iconEx = icons.querySelectorAll("[icon]"), a = 0; a < iconEx.length; a++) {
+            var iconClass = iconEx[a],
+                iconSvg = iconClass.getAttribute("icon"),
+                iconSpan = {
+                    "class": iconClass.className,
+                    size: iconClass.getAttribute("size")
+                };
+            iconClass.insertAdjacentHTML("beforebegin", t(iconSvg, iconSpan)), iconClass.parentNode.removeChild(iconClass)
+        }
+    }
+    icons.addEventListener("DOMContentLoaded", function() {
+        iconEx(), iconClass()
+    })
+}(window.document);
